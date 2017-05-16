e2463612c3efdd945ab7e759680630eeda82335c
ledoux <ledoux.florian30@gmail.com>
2017-05-16 20:48:59 +0200

    integration terminer de la nouvelle solution pour la carte,  ce qui et nouveau et que l'on peu copier le tableau de la classe <tableau_carte> mais de facon déférencé ce qui evitede modifier la list originale

--- package/carte.py
+++ package/carte.py
@@ -12,6 +12,20 @@ try:
 except:
 	import utilitaires as utils
 
+
+try:
+	import package.elements_de_carte as el_carte
+except:
+	import elements_de_carte as el_carte
+
+
+try:
+	import package.tableau_carte 
+except:
+	import tableau_carte
+
+import copy
+
 class Carte:
 	"""Objet de transition entre un fichier et un labyrinthe."""
 
@@ -28,8 +42,7 @@ class Carte:
 			raise TypeError("erreur le typage de la variable chaine doit etre de type <str> et non <{}>".format(type(chaine)))
 
 		self.nom = nom
-		self.labyrinthe = self.creer_labyrinthe_depuis_chaine(chaine)
-		self.bordure_labyrinthe()
+		self.labyrinthe = tableau_carte.Tableau_carte(self.creer_labyrinthe_depuis_chaine(chaine))
 
 
 
@@ -40,129 +53,132 @@ class Carte:
 
 	def __str__(self):
 		"""Méthode appelée quand on souhaite afficher la classe robot"""
-		chaine=str()
-		for x in self.labyrinthe:
-			for y in x:
-				chaine += y
 
-
-
-		msg = "{} \n <Carte: {}>".format(chaine, self.nom)
-		return msg
-
+		return "{}".format(self.labyrinthe)
 
 
 	def creer_labyrinthe_depuis_chaine(self,chaine):
-		""" je cree une list de list pour cree le labyrinthre"""
+		"""Méthode appelée quand on souhaite creer le labyrinthe"""
+		
+		#je cree une list de list pour cree le labyrinthre
 		i = chaine + "\n"
 		ligne = []
 		labyrinthe = []
 
-
+		#je remplit le tableau par des chaines de caracteres
 		for y,x in enumerate(i):
 
 			if x != "\n":
-				ligne.append(x)
+				if x == "O":
+					ligne.append(el_carte.Mur())
+				elif x == " ":
+					ligne.append(el_carte.Couloir())
+				elif x == ".":
+					ligne.append(el_carte.Porte())
+				elif x == "X":
+					ligne.append(el_carte.Joueur())
+				elif x == "U":
+					ligne.append(el_carte.Sortie())
+				else:
+					print("erreur lettre non reconnu par le programme")
 
 			else:
-				ligne.append(x)
 				labyrinthe.append(ligne)
 				ligne = []
-
 		return labyrinthe
-
-
-
-	def representation_labyrinthe_tableau(self):
-		"""representation du labyrinthe en tableau"""
-		for x in self.labyrinthe:
-			print(x)
-
-
-
-	def representation_labyrinthe_str(self, labyrinthe):
-		"""representation du labyrinthe en chaine de caractere"""
-		chaine=str()
-		for x in labyrinthe:
-			for y in x:
-				chaine += y
-		return chaine
 
 
 
 	def recherche_la_valeur_aux_coordonnees(self,coordonnee):
 		"""on recupere la valeur directement au coordonnee choisi"""
-		valeur = self.labyrinthe[coordonnee[0]][coordonnee[1]]
+		valeur = self.labyrinthe[(coordonnee[0],coordonnee[1])]
 		return valeur
 
 
 
-	def modifie_la_valeur_aux_coordonnees(self, labyrinthe, valeur, coordonneeYX):
+	def modifie_la_valeur_aux_coordonnees(self, valeur, coordonnee):
 		"""on modifie la valeur au coordonnée choisi"""
-		labyrinthe[coordonneeYX[0]][coordonneeYX[1]] = valeur
-		return labyrinthe
+
+		self.labyrinthe[(coordonnee[0],coordonnee[1])] = valeur
 
 
-	def recherche_les_coordonnees_des_valeurs(self,valeurs="X"):
-		"""retourne les coordonnee de la valeur trouvé.
-		par defaut la valeur a chercher c'est X"""
+
+	def recherche_les_coordonnees_des_valeurs(self, valeur):
+		"""retourne les coordonnee de la valeur trouvé."""
+
 		liste = list()
 		for j,y in enumerate(self.labyrinthe):
 			for v,x in enumerate(y):
-				if x == valeurs:
-					coord = (j, v)
-					liste.append(coord)
+					retour = self.labyrinthe[(j, v)]
+					if isinstance(retour, type(valeur)):
+						liste.append((j, v))
 		return liste
 
 
 
-
 	def bordure_labyrinthe(self):
+		"""Méthode appelée quand on souhaite creer une bordure sur l'extremité du labyrinthe"""
+
+		#je compte le nombre de case dans chaque ligne
 		nombre = []
 		for i, y in enumerate(self.labyrinthe):
 			nombre.insert(-1, len(self.labyrinthe[i]))
-
+		#je recupere le nombre de la plus grande ligne qui sera la valeur max
 		nombre = max(nombre)
 
 		for i, y in enumerate(self.labyrinthe):
-			self.labyrinthe[i].insert( 0, "O")
+			#j'insert a l'index zero de chaque ligne une bordure
+			self.labyrinthe.tableau[i].insert( 0, el_carte.Bordure())
 			j = len(self.labyrinthe[i])
+
 			while j <= nombre:
-				self.labyrinthe[i].insert( -1, " ")
-
+				#si les bordures sont irreguliere on rajoute des couloirs pour avoir une ligne droite
+				self.labyrinthe.tableau[i].append(el_carte.Couloir())
 				j += 1
-			self.labyrinthe[i][-1] = "O"
-			self.labyrinthe[i].append("\n")
+
+			#je rajoute la derniere bordure a la fin de chaque ligne
+			self.labyrinthe.tableau[i].append(el_carte.Bordure())
 
 
-		self.labyrinthe.insert( 0, self.bordure_principale( nombre + 2 ) )
-		self.labyrinthe.append(self.bordure_principale( nombre +2 ) )
-
+		self.labyrinthe.tableau.insert( 0, self.bordure_principale( nombre + 2 ) )
+		self.labyrinthe.tableau.append(self.bordure_principale( nombre +2 ) )
 
 
 
 	def bordure_principale(self, taille):
 		bordure = []
 		i = 0
-		while i < taille-1:
-			bordure.append("O")
+		while i <= taille-1:
+			bordure.append(el_carte.Bordure())
 			i += 1
-		bordure.append("\n")
 		return bordure
 
 
 
-	def carte_pour_utilisateur(self, coordonnee_utilisateur, valeur = "X"):
+	def modification_sur_copie_labyrinthe(self, coordonnee, copie_labyrinthe, valeur):
+		#print(copie_labyrinthe)
+		copie_labyrinthe[coordonnee[0]][coordonnee[1]] = valeur
+		#return copie_labyrinthe
+
+	#version avec le tableau du tableau
+
+	def carte_pour_utilisateur(self, coordonnee_utilisateur):
 		if not isinstance(coordonnee_utilisateur, tuple):
 			raise TypeError("erreur le typage de la variable coordonnee_utilisateur doit etre de type <tuple> et non <{}>".format(type(nom)))
-		labyrinthe = self.labyrinthe
-		liste_coordonnee = self.recherche_les_coordonnees_des_valeurs(valeur)
-		print(liste_coordonnee)
-		for coordonnee in liste_coordonnee:
-			if coordonnee != coordonnee_utilisateur:
-				labyrinthe = self.modifie_la_valeur_aux_coordonnees(labyrinthe, "x", coordonnee)
-		return labyrinthe
+		copie_labyrinthe = copy.deepcopy(self.labyrinthe.tableau)
 
+		liste_coordonnees = list(self.recherche_les_coordonnees_des_valeurs(el_carte.Joueur()))
+		print(coordonnee_utilisateur)
+		for coordonnee in liste_coordonnees:
+			if coordonnee != coordonnee_utilisateur:
+				self.modification_sur_copie_labyrinthe(coordonnee, copie_labyrinthe, el_carte.Autres_joueurs())
+
+		chaine=str()
+		for j,y in enumerate(copie_labyrinthe):
+			for v,x in enumerate(y):
+				chaine += str(copie_labyrinthe[j][v])
+			chaine +="\n"
+		return chaine
 
 
 
@@ -171,16 +187,28 @@ if __name__ == '__main__':
 	a = utils.chargement_donnee("/home/ledoux/Documents/Programmation/python/python-le-on/proj/rb_multi_joueur/cartes/")
 	labyrinthe = a[1]
 	nom_labyrinthe = a[0]
-	#print(labyrinthe, nom_labyrinthe)
-
 	a = Carte(nom_labyrinthe, labyrinthe)
-	a.representation_labyrinthe_tableau()
-	print(a)
-	b = a.recherche_les_coordonnees_des_valeurs()[0]
-	print(b)
-	print(a.recherche_la_valeur_aux_coordonnees((4, 9)))
-	print(a.representation_labyrinthe_str(a.carte_pour_utilisateur(coordonnee_utilisateur = (4, 9))))
+	#print(a)
+	#print(a.recherche_la_valeur_aux_coordonnees((0, 0)))
+	a.bordure_labyrinthe()
+	#print(a)
+	#a.modifie_la_valeur_aux_coordonnees(el_carte.Couloir(), (0, 0))
+	#print(a)
+	
+	#print(a.recherche_les_coordonnees_des_valeurs(el_carte.Joueur()))
+	print(a.carte_pour_utilisateur((4, 9)))
+	print(a.labyrinthe)
 	"""
-	Carte(a[0], a[1])
-	print(a)
+	i = [ ["0", "0", "0", "0", "0"], ["0", "0", "0", "0", "0"] ]
+	for j,y in enumerate(i):
+		for v,x in enumerate(y):
+			if x == "0":
+				i[j][v] = (el_carte.Mur((j, v)))
+	
+	for x in i:
+		for y in x:
+			print (y)
+		print("\n")
+	print
 	"""
+
--- package/elements_de_carte.py
+++ package/elements_de_carte.py
@@ -3,65 +3,78 @@
 
 class Elements_de_carte():
 	"""la classe Elements_de_carte et une classe de base pour les autres classe(Joueur, Mur, Bordure, Porte, Couloir)"""
-	def __init__(self, coordonnee):
-		if not isinstance(coordonnee, tuple):
-				raise TypeError("""Erreur lors de l'instanciation de la classe Robot!
-					le parametre fourni à coordonnee doit etre de type <tuple> et non <{}>""".format(type(coordonnee)))
+	def __init__(self):
 
-		for i in coordonnee:
-			if not isinstance(i,int):
-				raise TypeError("""Erreur lors de l'instanciation de la classe Robot!
-		argument dans list doit etre de type int et non <{}>""".format(type(i)))	
 
 		self.destructible = False
 		self.traversant = False
 		self.forme = ""
-		self.coordonnee = coordonnee
+		self.coordonnee = None
 
 
 
 	def __str__(self):
 		"""Méthode appelée quand on souhaite afficher la classe robot"""
-		msg = "destructible:{} traversant:{} forme:{} coordonnee:{}".format(self.destructible, self.traversant, self.forme, self.coordonnee)
+		msg = "{}".format(self.forme)
 		return msg
 
 
+	"""
+	def __getitem__():
+		print("coucou")
+		return self.forme
+	"""
+
 
 class Joueur(Elements_de_carte):
-	def __init__(self, coordonnee):
-		Elements_de_carte.__init__(self, coordonnee = coordonnee)
+	def __init__(self):
+		Elements_de_carte.__init__(self)
 		self.forme = "X"
-		self.element_avant_nouvelle_position = Couloir(coordonnee)
+		self.element_avant_nouvelle_position = Couloir()
+
+
+class Autres_joueurs(Elements_de_carte):
+	def __init__(self):
+		Elements_de_carte.__init__(self)
+		self.forme = "x"
 
 
 
 class Mur(Elements_de_carte):
-	def __init__(self, coordonnee):
-		Elements_de_carte.__init__(self,  coordonnee = coordonnee)
+	def __init__(self):
+		Elements_de_carte.__init__(self)
 		self.forme = "O"
 		self.destructible = True
 
 
 
 class Bordure(Elements_de_carte):
-	def __init__(self, coordonnee):
-		Elements_de_carte.__init__(self,  coordonnee = coordonnee)
+	def __init__(self):
+		Elements_de_carte.__init__(self)
 		self.forme = "O"
 
 
 
 class Porte(Elements_de_carte):
-	def __init__(self, coordonnee):
-		Elements_de_carte.__init__(self,  coordonnee = coordonnee)
+	def __init__(self):
+		Elements_de_carte.__init__(self)
 		self.forme = "."
 		self.traversant = True
 
 
 
 class Couloir(Elements_de_carte):
-	def __init__(self, coordonnee):
-		Elements_de_carte.__init__(self,  coordonnee = coordonnee)
+	def __init__(self):
+		Elements_de_carte.__init__(self)
 		self.forme = " "
+		self.traversant = True
+
+
+
+class Sortie(Elements_de_carte):
+	def __init__(self):
+		Elements_de_carte.__init__(self)
+		self.forme = "U"
 		self.traversant = True
 
 
@@ -71,11 +84,11 @@ if __name__ == "__main__":
 	#a = Elements_de_carte()
 	a = Joueur(coordonnee = (4, 2))
 	print(a)
-	a = Mur(coordonnee = (4, 2))
+	a = Mur()
 	print(a)
-	a = Bordure(coordonnee = (4, 2))
+	a = Bordure()
 	print(a)
-	a = Porte(coordonnee = (4, 2))
+	a = Porte()
 	print(a)
-	a = Couloir(coordonnee = (4, 2))
+	a = Couloir()
 	print(a)
