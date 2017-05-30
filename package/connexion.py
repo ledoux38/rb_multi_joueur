81a0d06199f696d327351268ff0855171a9b6cf3
ledoux <ledoux.florian30@gmail.com>
2017-05-30 21:47:43 +0200

    attribut robot obsolete remplacer par objet element_de_carte.Joueur()

--- package/connexion.py
+++ package/connexion.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3.5
 # -*-coding:Utf-8 -* 
-import socket as st
 
 try:
-	import package.robot as rt
+	import package.elements_de_carte as e_c
 except:
-	import robot as rt
-
+	import elements_de_carte as e_c
 
 class Connexion:
 
@@ -14,66 +12,36 @@ class Connexion:
 	avec position X, Y et une forme
 	mais aussi garder en mémoire la connexion ainsi que l'adresse (IP/ port)
 
-	ATTRIBUTS:	- Robot
+	ATTRIBUTS:	- Joueur
 				- information_connexion
 
 	"""
-	def __init__(self, coordonnee_xy_robot = (0, 0), forme_robot = "X", connexion = None):
+	def __init__(self, connexion = None):
 
 		"""instancie le Robot.
-		le robot est representer par une forme (X) et des coordonnes XY
+		le joueur est representer par une forme (X) et des coordonnes XY
 		"""
 
-		self.robot = rt.Robot(coordonnee_xy_robot, forme_robot)
-		print("type connexion:{}".format(type(connexion)))
-		self._information_connexion = connexion
+		self.joueur = e_c.Joueur()
+
+		self.information_connexion = connexion
 
 
 
 	def __str__(self):
-		"""Méthode appelée quand on souhaite afficher la classe robot"""
-		msg = "client{}: {}".format(self._information_connexion, self.robot)
+		"""Méthode appelée quand on souhaite afficher la classe joueur"""
+		
+		msg = "client {}: {}".format(self.information_connexion, self.joueur)
+		
 		return msg
-
-
-
-	def __del__(self):
-		"""Méthode appelée quand on souhaite supprimer la classe Connexion
-		l'objectif et de pouvoir fermer la connexion la classe"""
-		#self._information_connexion.close()
-		pass
-
-
-
-	def _set_information_connexion(self, connexion):
-		"""Méthode appelée quand on souhaite modifier les informations de connexion
-		mais comme je ne souhaite pas qu'elle soit modifiée je ne mais rien"""
-		if isinstance(self._information_connexion, None):
-			if not isinstance(connexion, st.socket):
-					raise TypeError("""Erreur dans la classe Connexion!
-									le parametre fourni à Connexion doit etre de type <socket> et non <{}>""".format(type(connexion)))
-		else:
-			pass
-
-
-
-	def _get_information_connexion(self):
-		print("connexion: ", type(self._information_connexion))
-		"""Méthode appelée quand on souhaite modifier la forme du robot"""
-		return self._information_connexion
-
-
-
-	information_connexion = property(fget = _get_information_connexion, fset = _set_information_connexion)
-
 
 
 
 if __name__ == "__main__":
 
 	a = Connexion()
-	print(a.robot)
+	print(a)
 	#a = Robot(coordonnee_XY = (2, 3))
-	#a.forme_robot = "Y"
-	b = a._information_connexion
+	#a.forme_joueur = "Y"
+	b = a.information_connexion
 	print(b)
